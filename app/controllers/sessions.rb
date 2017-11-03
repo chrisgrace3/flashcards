8189f1eed126f570ff3865f9071fa2fec3f848e3
AllyJo <gatorally245@gmail.com>
2017-11-03 16:42:05 -0500

    add session routes

---
+++ app/controllers/sessions.rb
@@ -0,0 +1,20 @@
+get '/sessions/new' do
+  erb :"sessions/new"
+end
+
+post '/sessions' do
+  @user = User.find_by(email: params[:email])
+
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @message = "Email or Password does not match. Please try again."
+    erb :"sessions/new"
+  end
+end
+
+delete '/sessions' do
+  session.clear
+  redirect '/'
+end
